143ecb5b13834405c0bb2b0a9db7b432e27ae3c0
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-10 00:26:18 +0300

    Add more styles

--- index.php
+++ index.php
@@ -10,25 +10,33 @@
 <body class="light">
     <!-- Header section -->
     <header class="header-section">
-        <div class="container d-flex">
-            <div class="w-100 d-flex mt-3 py-3">
-                <div class="d-flex p-2 justify-content-start align-items-start">
-                    Product List
+        <div class="container flex-grow-1 d-flex mt-4">
+            <div class="w-100 d-flex justify-content-between mt-3 top">
+                <div class="d-flex justify-content-start align-items-start">
+                    <span class="product-title">Product List</span>
                 </div>
-                <div class="ms-auto  d-flex justify-content-end align-items-start">
-                    <div class="p-2">Add</div>
-                    <div class="p-2">Mass delete</div>
+                <div class="ms-auto flex-row d-flex justify-content-between align-items-start">
+                    <div class="">
+                        <button class="btn">Add</button>
+                    </div>
+                    <div class="">
+                        <button class="btn"> Mass delete</button>
+                    </div>
                 </div>
             </div>
         </div>
     </header>
     
-    <section class="content-section">
+    <section class="content-section container">
+        <div class="w-100 d-flex content-section-products mt-2">
+            
 
+        </div>
     </section>
 
-    
+    <footer>
 
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
