220bc8d9b73104ba08114414189b481e06e6c3b4
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-09 23:26:34 +0300

    Add product sections

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Product List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="asset/dist/css/style.css">
-  </head>
-  <body>
-    <div class="container d-flex">
-        <div class="w-100 d-flex mt-3 py-3">
-            <div class="d-flex p-2 justify-content-start align-items-start">
-                Product List
+</head>
+<body class="light">
+    <!-- Header section -->
+    <header class="header-section">
+        <div class="container d-flex">
+            <div class="w-100 d-flex mt-3 py-3">
+                <div class="d-flex p-2 justify-content-start align-items-start">
+                    Product List
+                </div>
+                <div class="ms-auto  d-flex justify-content-end align-items-start">
+                    <div class="p-2">Add</div>
+                    <div class="p-2">Mass delete</div>
+                </div>
             </div>
-            <div class="ms-auto  d-flex justify-content-end align-items-start">
-                <div class="p-2">Add</div>
-                <div class="p-2">Mass delete</div>
-            </div>
-
         </div>
-    </div>
+    </header>
+    
+    <section class="content-section">
+
+    </section>
+
+    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
