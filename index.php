9470fc8f4fae15e22a0131acb7d9b6b75856a76f
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-10 16:24:40 +0300

    Add footer section

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
                 <div class="d-flex justify-content-start align-items-start py-2">
                     <span class="product-title">Product List</span>
                 </div>
-                <div class="ms-auto w-25 d-flex justify-content-between align-items-start py-2">
+                <div class="ms-auto group-button  d-flex justify-content-between align-items-start py-2">
                     <button class="btn btn-sm btn-default product-button-inline mx-2">
-                        <i class="fa fa-plus mx-2"></i>
+                        <i class="fa fa-plus mx-2 icon"></i>
                         <span>Add</span>
                     </button>
                     <button class="btn btn-sm btn-default product-button text-nowrap"> Mass delete</button>
@@ -35,7 +35,11 @@
     </section>
 
     <footer>
+        <div class="container">
+            <div class="d-flex justify-content-center border">
 
+            </div>
+        </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
