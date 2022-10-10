f426de1498ac7cf269e6e99a6211db20089090f0
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-10 23:25:34 +0300

    Add product

---
+++ addProduct.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Product List</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="asset/dist/css/style.css">
+</head>
+<body class="light">
+    <!-- Header section -->
+    <header class="header-section">
+        <div class="container flex-grow-1 d-flex mt-4">
+            <div class="w-100 d-flex justify-content-between mt-3 top">
+                <div class="d-flex justify-content-start align-items-start py-2">
+                    <span class="product-title">Product List</span>
+                </div>
+                <div class="ms-auto group-button  d-flex justify-content-between align-items-start py-2">
+                    <button class="btn btn-sm btn-default product-button-inline mx-2">
+                        <span>Add</span>
+                    </button>
+                    <button class="btn btn-sm btn-default product-button text-nowrap"> Mass delete</button>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <!-- Product List Section -->
+    <section class="content-section">
+        <div class="container">
+            <div class="w-100 content-section-products mt-2">
+                <div class="row mt-3">
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-md-3 mb-3">
+                        <div class="card product-box">
+                            <div class="d-flex align-items-center justify-content-center inner-box">
+                                <div class="form-check mt-2 check-form">
+                                    <input class="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault">
+                                </div>
+                                <div class="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                    <small class="card-text my-0">165 views - 58 hours ago</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer section -->
+    <footer>
+        <div class="container">
+            <div class="d-flex justify-content-center align-items-center footer-section">
+                <div class="align-self-center">Scandiweb Test assignment</div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+</body>
+</html>
