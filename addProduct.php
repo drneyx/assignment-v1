955cb37b51b108fbddc076959e8e84313b220f4d
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-11 00:07:12 +0300

    Add form attribute'

--- addProduct.php
+++ addProduct.php
@@ -32,51 +32,41 @@
             <div class="w-100 content-section-products mt-2">
                 <form>
                     <div class="row mb-3">
-                        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-6">
-                            <input type="email" class="form-control" id="inputEmail3">
+                        <label for="sku" class="col-sm-2 col-form-label">SKU</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" id="sku">
                         </div>
                     </div>
                     <div class="row mb-3">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                        <div class="col-sm-6">
-                            <input type="password" class="form-control" id="inputPassword3">
+                        <label for="name" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control" id="name">
                         </div>
                     </div>
-                    <fieldset class="row mb-3">
-                        <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-                        <div class="col-sm-10">
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-                            <label class="form-check-label" for="gridRadios1">
-                            First radio
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                            <label class="form-check-label" for="gridRadios2">
-                            Second radio
-                            </label>
-                        </div>
-                        <div class="form-check disabled">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-                            <label class="form-check-label" for="gridRadios3">
-                            Third disabled radio
-                            </label>
-                        </div>
-                        </div>
-                    </fieldset>
                     <div class="row mb-3">
-                        <div class="col-sm-10 offset-sm-2">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck1">
-                            <label class="form-check-label" for="gridCheck1">
-                            Example checkbox
-                            </label>
-                        </div>
+                        <label for="price" class="col-sm-2 col-form-label">Price($)</label>
+                        <div class="col-sm-4">
+                            <input type="number" class="form-control" id="price">
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+
+                    <div class="row mb-3">
+                        <label for="productType" class="col-sm-2 col-form-label">Type Switcher</label>
+                        <div class="col-sm-4">
+                            <select class="form-select" aria-label="Default select example" id="productType">
+                                <option selected>---select---</option>
+                                <option value="1">DVD</option>
+                                <option value="2">Furniture</option>
+                                <option value="3">Book</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="size" class="col-sm-2 col-form-label">size(MB)</label>
+                        <div class="col-sm-4">
+                            <input type="number" class="form-control" id="size">
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
